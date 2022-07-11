6d4329ad674d0930773332b4ffa9df97aaa269b0
Chad Jablonski <chad@jablonski.xyz>
2022-07-10 21:46:48 -0400

    Use cargo features

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,28 @@
 //! Libraries and tools for working with EverQuest game data
 //!
+//! # Examples
+//!
+//! ```rust
+//! use libeq::archive::EqArchive;
+//! use libeq::wld;
+//!
+//! fn main() {
+//!     // Extract .wld data from an .s3d file
+//!     let archive = EqArchive::read("gfaydark.s3d").unwrap();
+//!     let (_, data) = archive
+//!         .iter()
+//!         .find(|(name, _)| name == "gfaydark.wld")
+//!         .unwrap();
+//!
+//!     // Load .wld file
+//!     let wld = wld::load(data).unwrap();
+//!     let materials = wld.materials().collect::<Vec<_>>();
+//!     let meshes = wld.meshes().collect::<Vec<_>>();
+//!     let models = wld.models().collect::<Vec<_>>();
+//!     let objects = wld.objects().collect::<Vec<_>>();
+//! }
+//! ```
+//!
 //! # Crates
 //! * [libeq_wld](crates/libeq_wld) - Load `.wld` files.
 //! * [libeq_archive](crates/libeq_archive) - Create and extract `.s3d` archives.
@@ -7,5 +30,8 @@
 //! # Tools
 //! * [wld-cli](tools/wld-cli) - Command line tools for working with `.wld` files.
 //!
+
+#[cfg(feature = "archive")]
 pub use libeq_archive as archive;
+#[cfg(feature = "wld")]
 pub use libeq_wld as wld;
