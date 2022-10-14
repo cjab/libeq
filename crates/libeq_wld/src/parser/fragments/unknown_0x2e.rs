21c21b82208f5a4fd8af0c9d0bba44e3fee663d7
Bryan Fordney <126733+bryab@users.noreply.github.com>
2022-10-14 15:41:59 -0400

    WIP: RGBDEFORMATIONTRACKDEF

--- crates/libeq_wld/src/parser/fragments/unknown_0x2e.rs
+++ crates/libeq_wld/src/parser/fragments/unknown_0x2e.rs
@@ -1,17 +1,33 @@
-use std::any::Any;
-
 use super::{Fragment, FragmentParser, StringReference, WResult};
+use nom::multi::count;
+use nom::number::complete::{le_f32, le_u32};
+use nom::sequence::tuple;
+use std::any::Any;
 
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 #[derive(Debug, PartialEq)]
-/// Unknown
+/// RGBDEFORMATIONTRACKDEF
 ///
 /// **Type ID:** 0x2e
 pub struct Unknown0x2eFragment {
     pub name_reference: StringReference,
+
+    pub flags: u32,
+    // NUMVERTICES %d
+    pub vertex_count: u32,
+    // NUMFRAMES 1
+    pub frame_count: u32,
+    // SLEEP 200
+    pub sleep: u32,
+    pub param1: u32,
+    // RGBDEFORMATIONFRAME
+    //   NUMRGBAS %d
+    //   RGBA %9.7f, %9.7f, %9.7f, %9.7f
+    // ENDRGBDEFORMATIONFRAME
+    pub data: Vec<(f32, f32, f32)>,
 }
 
 impl FragmentParser for Unknown0x2eFragment {
@@ -23,7 +39,23 @@ impl FragmentParser for Unknown0x2eFragment {
     fn parse(input: &[u8]) -> WResult<Self> {
         let (i, name_reference) = StringReference::parse(input)?;
 
-        Ok((i, Self { name_reference }))
+        let (i, (flags, vertex_count, frame_count, sleep, param1)) =
+            tuple((le_u32, le_u32, le_u32, le_u32, le_u32))(i)?;
+
+        let (i, data) = count(tuple((le_f32, le_f32, le_f32)), (vertex_count * frame_count) as usize)(i)?;
+
+        Ok((
+            i,
+            Self {
+                name_reference,
+                flags,
+                vertex_count,
+                frame_count,
+                sleep,
+                param1,
+                data,
+            },
+        ))
     }
 }
 
