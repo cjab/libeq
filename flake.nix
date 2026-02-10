08947e9f4c5b77976fc5a517cce97af690695c9f
Chad Jablonski <chad@jablonski.xyz>
2026-02-09 22:06:05 -0500

    Update flakes and rust version

--- flake.nix
+++ flake.nix
@@ -3,96 +3,40 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    crate2nix = {
-      url = "github:kolloch/crate2nix";
-      flake = false;
-    };
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
   };
 
-  outputs = { self, nixpkgs, utils, rust-overlay, crate2nix, ... }:
+  outputs = { self, nixpkgs, rust-overlay }:
     let
-      name = "libeq";
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
     in
-    utils.lib.eachDefaultSystem
-      (system:
-        let
-          # Imports
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [
-              rust-overlay.overlays.default
-              (self: super: {
-                # Because rust-overlay bundles multiple rust packages into one
-                # derivation, specify that mega-bundle here, so that crate2nix
-                # will use them automatically.
-                rustc = self.rust-bin.nightly.latest.default;
-                cargo = self.rust-bin.nightly.latest.default;
-              })
-            ];
-          };
-          inherit (import "${crate2nix}/tools.nix" { inherit pkgs; })
-            generatedCargoNix;
-
-          # Create the cargo2nix project
-          project = pkgs.callPackage
-            (generatedCargoNix {
-              inherit name;
-              src = ./.;
-            })
-            {
-              # Individual crate overrides go here
-              # Example: https://github.com/balsoft/simple-osd-daemons/blob/6f85144934c0c1382c7a4d3a2bbb80106776e270/flake.nix#L28-L50
-              defaultCrateOverrides = pkgs.defaultCrateOverrides // {
-                # The app crate itself is overriden here. Typically we
-                # configure non-Rust dependencies (see below) here.
-                ${name} = oldAttrs: {
-                  inherit buildInputs nativeBuildInputs;
-                } // buildEnvVars;
-              };
+    {
+      devShells = forAllSystems
+        (system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ rust-overlay.overlays.default ];
             };
-
-          # Configuration for the non-Rust dependencies
-          buildInputs = with pkgs; [
-            pkg-config
-            openssl
-          ];
-          nativeBuildInputs = with pkgs; [
-            rustc
-            cargo
-            pkg-config
-            nixpkgs-fmt
-            rust-analyzer
-            perf-tools
-            cargo-flamegraph
-            cargo-readme
-          ];
-          buildEnvVars = { };
-        in
-        rec {
-          packages.${name} = project.workspaceMembers.eq_client.build;
-
-          # `nix build`
-          defaultPackage = packages.${name};
-
-          # `nix run`
-          apps.${name} = utils.lib.mkApp {
-            inherit name;
-            drv = packages.${name};
-          };
-          defaultApp = apps.${name};
-
-          # `nix develop`
-          devShell = pkgs.mkShell
-            {
-              inherit buildInputs nativeBuildInputs;
-              RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-            } // buildEnvVars;
-        }
-      );
+            rust = pkgs.rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" "rust-analyzer" ];
+            };
+          in
+          {
+            default = pkgs.mkShell {
+              buildInputs = [
+                rust
+                pkgs.pkg-config
+                pkgs.openssl
+              ];
+              nativeBuildInputs = [
+                pkgs.nixpkgs-fmt
+                pkgs.perf-tools
+                pkgs.cargo-flamegraph
+                pkgs.cargo-readme
+              ];
+            };
+          });
+    };
 }
