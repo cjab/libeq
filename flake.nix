e06ae17dd38b624983663505e25b99a259ea0b15
Chad Jablonski <chad@jablonski.xyz>
2026-03-02 14:46:30 -0500

    Add s3d and wld-cli to flake packages

--- flake.nix
+++ flake.nix
@@ -4,39 +4,85 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { self, nixpkgs, rust-overlay }:
+  outputs = { self, nixpkgs, rust-overlay, crane }:
     let
       systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs systems;
+      forSystem = system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ rust-overlay.overlays.default ];
+          };
+          rust = pkgs.rust-bin.stable.latest.default;
+          rustDev = rust.override {
+            extensions = [ "rust-src" "rust-analyzer" ];
+          };
+          craneLib = (crane.mkLib pkgs).overrideToolchain rust;
+
+          src = pkgs.lib.cleanSourceWith {
+            src = ./.;
+            filter = path: type:
+              (craneLib.filterCargoSources path type)
+              || builtins.match ".*README\\.md$" path != null;
+          };
+
+          commonArgs = {
+            inherit src;
+            strictDeps = true;
+          };
+
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        in
+        {
+          inherit pkgs rustDev craneLib commonArgs cargoArtifacts;
+        };
     in
     {
-      devShells = forAllSystems
-        (system:
-          let
-            pkgs = import nixpkgs {
-              inherit system;
-              overlays = [ rust-overlay.overlays.default ];
-            };
-            rust = pkgs.rust-bin.stable.latest.default.override {
-              extensions = [ "rust-src" "rust-analyzer" ];
-            };
-          in
-          {
-            default = pkgs.mkShell {
-              buildInputs = [
-                rust
-                pkgs.pkg-config
-                pkgs.openssl
-              ];
-              nativeBuildInputs = [
-                pkgs.nixpkgs-fmt
-                pkgs.perf-tools
-                pkgs.cargo-flamegraph
-                pkgs.cargo-readme
-              ];
-            };
+      overlays.default = final: prev: {
+        s3d = self.packages.${final.system}.s3d;
+        wld-cli = self.packages.${final.system}.wld-cli;
+      };
+
+      packages = forAllSystems (system:
+        let
+          s = forSystem system;
+        in
+        {
+          s3d = s.craneLib.buildPackage (s.commonArgs // {
+            inherit (s) cargoArtifacts;
+            pname = "s3d";
+            cargoExtraArgs = "--package s3d";
           });
+
+          wld-cli = s.craneLib.buildPackage (s.commonArgs // {
+            inherit (s) cargoArtifacts;
+            pname = "wld-cli";
+            cargoExtraArgs = "--package wld-cli";
+          });
+        });
+
+      devShells = forAllSystems (system:
+        let
+          s = forSystem system;
+        in
+        {
+          default = s.pkgs.mkShell {
+            buildInputs = [
+              s.rustDev
+              s.pkgs.pkg-config
+              s.pkgs.openssl
+            ];
+            nativeBuildInputs = [
+              s.pkgs.nixpkgs-fmt
+              s.pkgs.perf-tools
+              s.pkgs.cargo-flamegraph
+              s.pkgs.cargo-readme
+            ];
+          };
+        });
     };
 }
